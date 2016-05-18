3122831c4a041ea462b461ad67e16de1b0ed0a52
AtanasBeshovishki <atanas@next-stream.com>
2016-05-18 10:39:57 +0300

    Added files

---
+++ TwoDates/TwoDates/TwoDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace TwoDates
+{
+    class TwoDates
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+           "d.M.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1.0))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                    date.DayOfWeek == DayOfWeek.Sunday)
+                    holidaysCount++;
+            }
+                
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
